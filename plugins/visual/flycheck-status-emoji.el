d455bdea8aaaa5f1185fcf3f8a4a0a96d9408a30
nameless <4eam3less@gmail.com>
2022-02-13 18:04:16 +0300

    Изменены смайлики для плагина flycheck-status-emoji

--- plugins/visual/flycheck-status-emoji.el
+++ plugins/visual/flycheck-status-emoji.el
@@ -1,7 +1,15 @@
 (use-package flycheck-status-emoji)
 
+(if (not (find-font (font-spec :name "Noto color emoj")))
+    (all-the-icons-install-fonts t))
+
 (flycheck-status-emoji-mode 1)
+(setq flycheck-status-emoji-indicator-finished-ok 128571)
+(setq flycheck-status-emoji-indicator-finished-error 128576)
+(setq flycheck-status-emoji-indicator-finished-warning 128574)
+(setq flycheck-status-emoji-indicator-finished-info 128570)
+(setq flycheck-status-emoji-indicator-suspicious 128572)
 
 ;; Для отображения этого мода нужно что-бы шрифт поддерживал эмодзи
 (set-fontset-font t '(#x1f000 . #x1faff)
-		      (font-spec :family "Apple Color Emoji"))
+		      (font-spec :family "Noto color emoji"))
