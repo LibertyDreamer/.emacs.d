b4e4997057a7ea1d182f1015c65b0f9384081464
Nikita <nikitadotivanov64@gmail.com>
2022-03-21 09:31:01 +0300

    -

--- plugins/visual/dashboard.el
+++ plugins/visual/dashboard.el
@@ -7,10 +7,11 @@
 
 (setq dashboard-startup-banner "~/.emacs.d/dashboard-image/marivector.png")
 
-(add-hook 'dashboard-mode-hook
-	  (lambda ()
-	    (delete-other-windows)
-	    )
-	  )
+;; Вызывет ошибку при старте
+;; (add-hook 'dashboard-mode-hook
+;; 	  (lambda ()
+;; 	    (delete-other-windows)
+;; 	    )
+;; 	  )
 
 ;;; dashboard.el ends here
