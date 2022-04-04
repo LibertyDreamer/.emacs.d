405407c6a5df502c98885ef2615ab2122f06a72f
Nikita <nikitadotivanov64@gmail.com>
2022-04-04 16:25:53 +0300

    Теперь емак будет хранить весь список изменений файла. т.е. undo-tree будет работать и после перезапуска.

--- plugins/utilities/undo-tree.el
+++ plugins/utilities/undo-tree.el
@@ -9,7 +9,10 @@
 ;;; Позволяет отобразить дерево изменений файла.
 (use-package undo-tree
   :config
-  ;;; Использовать этот мод везде
+  ;;; Сохранять историю изменений для уиспользывания после
+  ;;; перезапуска emacs.
+  (setq undo-tree-auto-save-history t)
+  ;;; Использовать этот мод везде.
   (global-undo-tree-mode))
 
 ;;; undo-tree.el ends here
