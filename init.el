3ce4702940ee1c5bf05d03909007bd1c3290d9e7
LibertyDreamer <hershtainalbert@gmail.com>
2024-06-18 18:27:39 +0200

    Update

--- init.el
+++ init.el
@@ -1,3 +1,41 @@
+;; Core application 
+;; Without them all will be broken
+;;For start let's install base applications
+
+;;Installing straight.el. I don't understand what is going on,
+;;I simply copied it from straight installation instruction.
+
+  (defvar bootstrap-version)
+  (let ((bootstrap-file
+	 (expand-file-name
+	  "straight/repos/straight.el/bootstrap.el"
+	  (or (bound-and-true-p straight-base-dir)
+	      user-emacs-directory)))
+	(bootstrap-version 7))
+    (unless (file-exists-p bootstrap-file)
+      (with-current-buffer
+	  (url-retrieve-synchronously
+	   "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+	   'silent 'inhibit-cookies)
+	(goto-char (point-max))
+	(eval-print-last-sexp)))
+    (load bootstrap-file nil 'nomessage))
+
+;; Use straight for use-package expressions,
+;; Non-nil straight-use-package-by-default means install packages
+;; by default in ‘use-package’ forms.
+
+  (straight-use-package 'use-package)
+  (setq straight-use-package-by-default t)
+
+;; Next, I use literate config I want to have auto tangle
+  (use-package org-auto-tangle
+    :hook (org-mode . org-auto-tangle-mode))
+
+
+
+
+
 (load-file "~/.emacs.d/configuration.el")
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
