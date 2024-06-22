db0ebf8b4a486d983c6813e17e87e0618fe30443
LibertyDreamer <hershtainalbert@gmail.com>
2024-06-23 00:02:16 +0200

    update

--- init.el
+++ init.el
@@ -4,32 +4,38 @@
 
 ;;Installing straight.el. I don't understand what is going on,
 ;;I simply copied it from straight installation instruction.
-
-  (defvar bootstrap-version)
-  (let ((bootstrap-file
-	 (expand-file-name
-	  "straight/repos/straight.el/bootstrap.el"
-	  (or (bound-and-true-p straight-base-dir)
-	      user-emacs-directory)))
-	(bootstrap-version 7))
-    (unless (file-exists-p bootstrap-file)
-      (with-current-buffer
-	  (url-retrieve-synchronously
-	   "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-	   'silent 'inhibit-cookies)
-	(goto-char (point-max))
-	(eval-print-last-sexp)))
-    (load bootstrap-file nil 'nomessage))
+;;; Code:
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name
+	"straight/repos/straight.el/bootstrap.el"
+	(or (bound-and-true-p straight-base-dir)
+	    user-emacs-directory)))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+	(url-retrieve-synchronously
+	 "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+	 'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
 ;; Use straight for use-package expressions,
 ;; Non-nil straight-use-package-by-default means install packages
 ;; by default in ‘use-package’ forms.
-
-  (straight-use-package 'use-package)
-  (setq straight-use-package-by-default t)
+(straight-use-package 'use-package)
+(setq straight-use-package-by-default t)
 
 ;; Next, I use literate config I want to have auto tangle
-  (use-package org-auto-tangle
-    :hook (org-mode . org-auto-tangle-mode))
+(use-package org-auto-tangle
+  :hook (org-mode . org-auto-tangle-mode))
+
+(defmacro package! (name &rest args)
+  "A simple macro to declare packages similar to Doom Emacs' `package!`."
+  `(use-package ,name
+     ,@args))
 
 (load-file "~/.emacs.d/configuration.el")
+
+;;; init.el ends here
